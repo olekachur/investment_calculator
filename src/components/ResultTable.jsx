f1c262cad1cfd099265e2ab1dd79222ce9dba3f7
OlexandrKachur <lexandrkachur@gmail.com>
2024-09-24 21:06:02 +0300

    calculating Total Interest and Invested Capital

--- src/components/ResultTable.jsx
+++ src/components/ResultTable.jsx
@@ -1,8 +1,8 @@
 import { calculateInvestmentResults, formatter } from "../util/investment";
 
-
 export default function ResultTable({ investmentResult }) {
     const results = calculateInvestmentResults(investmentResult);
+    const initialInvest = results[0].valueEndOfYear - results[0].interest - results[0].annualInvestment;
 
     return (
         <table id="result">
@@ -16,15 +16,17 @@ export default function ResultTable({ investmentResult }) {
                 </tr>
             </thead>
             <tbody>
-                {results.map((result) => (
-                    <tr className="center" key={result.year}>
+                {results.map((result) => {
+                    const totalInvest = result.valueEndOfYear - result.annualInvestment * result.year - initialInvest;
+                    const toalAmountInvest = result.valueEndOfYear - totalInvest;
+                    return (<tr className="center" key={result.year}>
                         <th>{result.year}</th>
                         <th>{formatter.format(result.valueEndOfYear)}</th>
                         <th>{formatter.format(result.interest)}</th>
-                        <th>{formatter.format(result.interest * result.year)}</th>
-                        <th>{formatter.format(result.annualInvestment * result.year)}</th>
-                    </tr>
-                ))}
+                        <th>{formatter.format(totalInvest)}</th>
+                        <th>{formatter.format(toalAmountInvest)}</th>
+                    </tr>)
+                })}
             </tbody>
         </table>
     );
